4bff488848bfccf9c760847079c4fbcb3fe2b607
LillardW <LillardW@users.noreply.github.com>
2018-03-14 21:41:38 +0800

    Add thymeleaf support and templates.

--- src/main/java/com/flightticketsystem/runtime/RuntimeApplication.java
+++ src/main/java/com/flightticketsystem/runtime/RuntimeApplication.java
@@ -2,6 +2,7 @@ package com.flightticketsystem.runtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @ComponentScan(basePackages = {"com.flightticketsystem.runtime.service", "com.flightticketsystem.runtime.controller"})
 public class RuntimeApplication {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RuntimeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RuntimeApplication.class, args);
---
+++ src/main/java/com/flightticketsystem/runtime/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.flightticketsystem.runtime.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/index")
+    public String homePage(){
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+}
